869003edca20e1998cc7b047598ec33576d36cd8
Barry Warnock <brrwarnock@gmail.com>
2017-04-01 21:59:45 -0700

    redid the threading
    I used python thread executors in favour of pymp which is a bit messier but quite a bit faster.
    It's still unacceptably slow. Will probably end up rewriting in c++, glut code and opengl code should port directly. It basically comes down to finding a c++ image library I'm happy with.

--- pixellate.py
+++ pixellate.py
@@ -4,6 +4,7 @@ from pix_opengl import init_opengl, set_texture, draw
 import time
 import pymp
 import numpy
+from concurrent.futures import *
 
 pymp.config.nested = True
 
@@ -21,28 +22,51 @@ chunkHeight = int(height/rows)
 init_opengl(width, height)
 set_texture(image)
 
-for i in range(frames):
-    count = pymp.shared.array((columns, rows), dtype='uint8')
-    avgs  = pymp.shared.list([[[0,0,0] for j in range(rows)] for i in range(columns)])#list comprehension is pretty neat
-    with pymp.Parallel(columns) as column:
-        with pymp.Parallel(rows) as row:
-            chunkStartX = column.thread_num * chunkWidth
-            chunkStartY = row.thread_num * chunkHeight
-            for x in range(chunkStartX, chunkStartX+chunkWidth):
-                for y in range(chunkStartY, chunkStartY+chunkHeight):
-                                count[column.thread_num,row.thread_num] += 1
-                                pixel = image.getpixel((x,y))
-                                avgs[column.thread_num][row.thread_num][0] += pixel[0]
-                                avgs[column.thread_num][row.thread_num][1] += pixel[1]
-                                avgs[column.thread_num][row.thread_num][2] += pixel[2]
-
-    for x in range(columns):
-        for y in range(rows):
-            avgs[x][y][0] = avgs[x][y]/count[x,y][0]
-            avgs[x][y][0] = avgs[x][y]/count[x,y][0]
-            avgs[x][y][0] = avgs[x][y]/count[x,y][0]
-            print(avgs[x][y],end="")
-        print()
+def do_chunk(column, row):
+    chunkStartX = column * chunkWidth
+    chunkStartY = row * chunkHeight
+    count = 0
+    average = [0,0,0]
     
+    for x in range(chunkStartX, chunkStartX+chunkWidth):
+        for y in range(chunkStartY, chunkStartY+chunkHeight):
+            count += 1
+            pixel = image.getpixel((x,y))
+            average[0] += pixel[0]
+            average[1] += pixel[1]
+            average[2] += pixel[2]
+    return (column, row), [int(average[i]/count) for i in range(3)]
+
+
+def do_rows(column, rows):
+    with ThreadPoolExecutor(max_workers=rows) as executor:
+        rowThreads = []
+        for row in range(rows):
+            rowThreads.append(executor.submit(do_chunk, column, row))
+
+        rowValues = [0 for i in range(rows)]
+        for rowThread in rowThreads:
+            coordinate, average = rowThread.result()
+            rowValues[coordinate[1]] = average
+        
+        return column, rowValues
+        
+
+def average_image(columns, rows):
+    with ThreadPoolExecutor(max_workers=columns) as executor:
+        columnThreads = []
+        for column in range(columns):
+            columnThreads.append(executor.submit(do_rows, column, rows))
+
+        columnValues = [0 for i in range(columns)]
+        for columnThread in columnThreads:
+            coordinate, rowsValues = columnThread.result()
+            columnValues[coordinate] = rowsValues
+
+        return columnValues
+
+
+for i in range(frames):
+    print(average_image(columns, rows))
     draw()
     time.sleep(1)
